6d4d8f5962301d31ac400f27077a735b07868ca7
Deleranax <deleranax@gmail.com>
2023-10-01 16:52:39 +0200

    Updating TAC

--- tac/server/apis/tac-server-command.lua
+++ tac/server/apis/tac-server-command.lua
@@ -131,7 +131,8 @@ local function com(command, args)
         end
     elseif command == "users" then
         vui.console("-- Users --")
-        vui.console(string.format("%20s | %4s", "Name", "Perm"))
+        vui.console(string.format("%20s | %4s", "Name of the User", "Perm"))
+        vui.console(string.format("---------------------+-----"))
         for k, v in pairs(_G.tacTemp.database.users) do
             vui.console(string.format("%20s | %4d", v.name, v.perm))
         end
@@ -162,7 +163,8 @@ local function com(command, args)
         end
     elseif command == "actions" then
         vui.console("-- Actions --")
-        vui.console(string.format("%20s | %4s | %4s | %4s", "Name", "Perm", "ID", "Dest"))
+        vui.console(string.format("%20s | %4s | %4s | %4s", "Name of the Action", "Perm", "ID", "Dest"))
+        vui.console(string.format("---------------------+------+-----+-----"))
         for k, v in pairs(_G.tacTemp.database.actions) do
             vui.console(string.format("%20s | %4d | %4d | %4d", k, v.perm, v.id, v.dest))
         end
